ef23abe845f445c87b5af186a88250515f6e7a86
ghutterer <gregor.hutterer@hotmail.com>
2022-01-05 17:37:54 +0100

    Finished

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParsingException {
         System.out.println("Hello Parser");
+        Parser parser1 = new Parser();
+        parser1.parseFile("sample1.txt");
+
     }
 }
--- src/main/java/application/Parser.java
+++ src/main/java/application/Parser.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -8,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Parser {
@@ -19,6 +22,27 @@ public class Parser {
     }
 
     public void parseFile(final String filename) throws ParsingException {
+
+        File file = new File(getClass().getClassLoader().getResource(filename).getFile());
+        try(Scanner input = new Scanner(file)){
+            while(input.hasNextLine()){
+                String str = input.nextLine();
+                System.out.println(str);
+                for (int i =0; i<str.length(); i++){
+                    elementStack.add(str.charAt(i));
+                }
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File not found lol");
+        }
+
+        int openp = (int)elementStack.stream().filter(x->x.equals('{')).count();
+        int closep = (int)elementStack.stream().filter(x->x.equals('}')).count();
+        if(openp>closep)throw new ParsingException("Error parsing the file. } expected");
+        if(openp<closep)throw new ParsingException("Error parsing the file. { expected");
+        System.out.println(openp);
+        System.out.println(closep);
+
     }
 
     public Deque<Character> getElementStack() {
